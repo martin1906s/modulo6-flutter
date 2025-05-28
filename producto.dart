802384fd65794a793ea25019b884c4a34f01fdf3
martin1906s <martin.simbana007@gamil.com>
2025-05-27 21:33:19 -0500

    Reto 1

---
+++ producto.dart
@@ -0,0 +1,14 @@
+class Producto {
+  String codigo = '';
+  String Nombre = '';
+  String? descripcion;
+  bool activo = true;
+  double precio = 0.0;
+  int? stock;
+}
+
+void main() {
+  Producto p1 = new Producto();
+  Producto p2 = Producto();
+  final p3 = Producto();
+}
