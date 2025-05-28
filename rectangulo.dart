fbd36373590a08aea9ba8f71150c555ced916761
martin1906s <martin.simbana007@gamil.com>
2025-05-27 21:36:59 -0500

    Reto 2

---
+++ rectangulo.dart
@@ -0,0 +1,27 @@
+class Rectangulo{
+  int base=0;
+  int altura=0;
+
+  int calcularArea(){
+    return base*altura;
+  }
+  int calcularPerimetro(){
+    return 2*base+2*altura;
+  }
+
+  Rectangulo(int base, int altura){
+    this.base = base;
+    this.altura = altura;
+  }
+}
+
+void main(){
+  final r1 = Rectangulo(5,10);
+  int area1 = r1.calcularArea();
+
+  final r2 = Rectangulo(8,2);
+  int area2 = r2.calcularArea();
+
+  print(area1);
+  print(area2);
+}
